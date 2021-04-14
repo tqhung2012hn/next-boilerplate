e17b4c163981c8d5f103e7c6bd6753aa5c1da59b
Trần Quang Hùng <tranquang.hung@etc.vn>
2021-04-14 14:03:50 +0700

    Restructure the project folders

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
+
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.object.isRequired
+};
+
+export default MyApp;
