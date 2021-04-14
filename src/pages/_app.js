d2d0c95ef5f591de23a1760f186864582a4e976c
Trần Quang Hùng <tranquang.hung@etc.vn>
2021-04-14 14:26:14 +0700

    Setting up Styled-Components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,25 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles, theme } from '@/styles/GlobalStyles';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
 MyApp.propTypes = {
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components';
+
+const theme = {};
+
+const GlobalStyles = createGlobalStyle``;
+
+export { GlobalStyles, theme };
