fca696666dc236ed8b6a047bd17f05885791e63b
Trần Quang Hùng <tranquang.hung@etc.vn>
2021-04-14 13:21:02 +0700

    Setting up ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    env: {
+        browser: true, // Enables browser globals like window and document.
+        node: true, // Enables Node.js global variables and Node.js scoping.
+        es6: true // Enables ES6 syntax.
+    },
+    plugins: ['import'],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+    rules: {
+        'react/forbid-prop-types': 0,
+        'react/jsx-filename-extension': 0,
+        'react/react-in-jsx-scope': 0,
+        'react/jsx-props-no-spreading': [
+            0,
+            { html: 'ignore', custom: 'ignore', explicitSpread: 'ignore' }
+        ],
+        'react-hooks/exhaustive-deps': 0,
+        'import/no-unresolved': 'error', // Turn on errors for missing imports.
+        'import/prefer-default-export': 0,
+        'import/order': [
+            2,
+            {
+                groups: [
+                    'builtin',
+                    'external',
+                    'internal',
+                    'parent',
+                    'sibling',
+                    'index'
+                ],
+                'newlines-between': 'always'
+            }
+        ],
+        'import/newline-after-import': 1,
+        'jsx-a11y/anchor-is-valid': [
+            'error',
+            {
+                components: ['Link'],
+                specialLink: ['hrefLeft', 'hrefRight'],
+                aspects: ['invalidHref', 'preferButton']
+            }
+        ],
+        'jsx-a11y/label-has-associated-control': [
+            2,
+            {
+                labelComponents: ['label']
+            }
+        ]
+    }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 80,
+    endOfLine: 'auto',
+    trailingComma: 'none',
+    semi: true,
+    useTabs: false,
+    singleQuote: true,
+    bracketSpacing: true,
+};
